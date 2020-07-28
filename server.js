9fced2d90873ed69d74539d7b2ef060a79b073be
vankatwijk <hendrikus.hpvk@gmail.com>
2020-07-28 17:05:41 +0200

    body parser added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const methodOverride = require('method-override')
+const bodyParser = require('body-parser'); //package for getting url encodings GET POST request
 const app = express()
 const carbone = require('carbone')
 
@@ -21,8 +22,23 @@ app.get('/', async (req, res) =>{
 
 
 app.get('/generate', async (req, res) =>{
+    var options = {};
+    var ext = 'odt'
+    console.log(req.body)
+    if(req.body.options){
+        options = {
+            convertTo : req.body.options //can be docx, txt, ...
+        };
+        ext = req.body.options
+    }
 
-    carbone.render('./node_modules/carbone/examples/simple.odt', data, function(err, result){
+    // Data to inject
+    var data = {
+        firstname : 'John',
+        lastname : 'Doe'
+    };
+
+    carbone.render('./node_modules/carbone/examples/simple.odt', data, options, function(err, result){
         if (err) {
           return console.log(err);
         }
@@ -31,7 +47,9 @@ app.get('/generate', async (req, res) =>{
 
         res.status(200).json({
             message: 'Success',
-            pdfbinary: result
+            ext,
+            body:req.body,
+            result: result
         });
 
       });
